b468e001bd838bd5dbe2aa39d2b6d5d9b7ef1ed6
Liam-Alves <lilianalves.magalhaes@gmail.com>
2024-06-30 13:09:32 +0200

    mise à jour allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/", "galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/", "lacarte", "La carte", "/pages/lacarte.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/lacarte", "La carte", "/pages/lacarte.html"),
+    new Route("/lesreservations", "Les réservations", "/pages/lesreservation.html"),
+    new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),
+    new Route("/connexion", "Connexion / Deconnexion", "/pages/connexion.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
