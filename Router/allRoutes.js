067167bac63297eaecec9eea00f90092c9927aa6
Liam-Alves <lilianalves.magalhaes@gmail.com>
2024-07-17 18:35:22 +0200

    mise à jour page réserver

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -11,6 +11,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
